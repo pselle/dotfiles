3ef6ed8272fc0690d932008578280b29ca246f41
Mike Ball <mikedball@gmail.com>
2015-05-06 07:45:50 -0400

    set up zsh installation

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ mv ${HOME}/.bashrc ${HOME}/.bashrc.bak 2>/dev/null
 # ln -s ${DOTFILES}/bash_profile ${HOME}/.bash_profile
 # ln -s ${DOTFILES}/bashrc ${HOME}/.bashrc
 # ln -s ${DOTFILES}/profile ${HOME}/.profile
-ln -s ${DOTFILES}/profile ${HOME}/.zshrc
+ln -s ${DOTFILES}/zshrc ${HOME}/.zshrc
+ln -s ${DOTFILES}/oh-my-zsh/themes/mdb.zsh-theme ${HOME}/.oh-my-zsh/themes/mdb.zsh-theme
 ln -s ${DOTFILES}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${DOTFILES}/gitconfig ${HOME}/.gitconfig
 ln -s ${DOTFILES}/vimrc ${HOME}/.vimrc
