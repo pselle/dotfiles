14dc8778640e28e773e2d3856fda547e6014cbbf
Mike Ball <mikedball@gmail.com>
2015-05-13 08:20:34 -0400

    correct java 8 path

--- jdk.sh
+++ jdk.sh
@@ -1,5 +1,5 @@
 export JAVA_6_HOME=/System/Library/Frameworks/JavaVM.framework/Home
-export JAVA_8_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home
+export JAVA_8_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home
 
 export JAVA_HOME=$JAVA_8_HOME
 
