143d19c0552c31d3a825094f825bd055173b0c0a
Pam Selle <pamela.selle@gmail.com>
2019-03-25 15:25:07 -0400

    Clean up things I don't use

--- install.sh
+++ install.sh
@@ -8,14 +8,6 @@ mv ${HOME}/.bashrc ${HOME}/.bashrc.bak 2>/dev/null
 # ln -s ${DOTFILES}/bash_profile ${HOME}/.bash_profile
 # ln -s ${DOTFILES}/bashrc ${HOME}/.bashrc
 ln -s ${DOTFILES}/profile ${HOME}/.profile
-ln -s ${DOTFILES}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${DOTFILES}/gitconfig ${HOME}/.gitconfig
-ln -s ${DOTFILES}/vimrc ${HOME}/.vimrc
-ln -s ${DOTFILES}/vimrc.bundles ${HOME}/.vimrc.bundles
-ln -s ${DOTFILES}/vim ${HOME}/.vim
 ln -s ${DOTFILES}/bin ${HOME}/bin
 
-git clone https://github.com/gmarik/Vundle.vim.git ${DOTFILES}/vim/bundle/Vundle.vim
-
-# Install Vundles
-vim +PluginInstall +qall
