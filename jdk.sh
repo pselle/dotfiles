edfeaec1d41f67f5596036ead5e2f7628d91deb1
Mike Ball <mikedball@gmail.com>
2019-11-14 06:40:12 -0500

    update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -1,7 +1,10 @@
 export JAVA_6_HOME=/System/Library/Frameworks/JavaVM.framework/Home
 export JAVA_8_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home
+export JAVA_11_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home
 
-export JAVA_HOME=$JAVA_8_HOME
+export JAVA_HOME=$JAVA_11_HOME
+export M2_HOME=/usr/local/Cellar/maven/3.6.0/libexec
+export PATH=$PATH:$M2_HOME/bin
+export M2=$M2_HOME/bin
 
-alias java6='export JAVA_HOME=$JAVA_6_HOME'
-alias java8='export JAVA_HOME=$JAVA_8_HOME'
+alias java11='export JAVA_HOME=$JAVA_11_HOME'
