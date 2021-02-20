cfcf6a6597178a9fcd8a061f512ef095ccbe9b01
Mike Ball <mikedball@gmail.com>
2021-02-20 06:39:55 -0500

    add Brewfile & appease shellcheck

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+brew bundle
+
 DOTFILES=${HOME}/dotfiles
 
 # Move default bashrc out of the way
-mv ${HOME}/.bashrc ${HOME}/.bashrc.bak 2>/dev/null
+mv "${HOME}/.bashrc" "${HOME}/.bashrc.bak" 2>/dev/null
 
-ln -s ${DOTFILES}/zshrc ${HOME}/.zshrc
-ln -s ${DOTFILES}/oh-my-zsh/themes/mdb.zsh-theme ${HOME}/.oh-my-zsh/themes/mdb.zsh-theme
-ln -s ${DOTFILES}/tmux.conf ${HOME}/.tmux.conf
-ln -s ${DOTFILES}/gitconfig ${HOME}/.gitconfig
-ln -s ${DOTFILES}/vimrc ${HOME}/.vimrc
-ln -s ${DOTFILES}/jshintrc ${HOME}/.jshintrc
+ln -s "${DOTFILES}/zshrc" "${HOME}/.zshrc"
+ln -s "${DOTFILES}/oh-my-zsh/themes/mdb.zsh-theme" "${HOME}/.oh-my-zsh/themes/mdb.zsh-theme"
+ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
+ln -s "${DOTFILES}/gitconfig" "${HOME}/.gitconfig"
+ln -s "${DOTFILES}/vimrc" "${HOME}/.vimrc"
+ln -s "${DOTFILES}/jshintrc" "${HOME}/.jshintrc"
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
