740b65610334cc355218240940404fffe88e2227
Mike Ball <mikedball@gmail.com>
2021-04-24 08:37:55 -0400

    update installation to install homebrew

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Install homebrew if it's not already installed.
+# This should be compared to the latest installation
+# instructions at https://brew.sh before executing.
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 brew bundle
 
 DOTFILES=${HOME}/dotfiles
-
-# Move default bashrc out of the way
-mv "${HOME}/.bashrc" "${HOME}/.bashrc.bak" 2>/dev/null
 
 ln -s "${DOTFILES}/zshrc" "${HOME}/.zshrc"
 ln -s "${DOTFILES}/oh-my-zsh/themes/mdb.zsh-theme" "${HOME}/.oh-my-zsh/themes/mdb.zsh-theme"
@@ -13,6 +17,12 @@ ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
 ln -s "${DOTFILES}/gitconfig" "${HOME}/.gitconfig"
 ln -s "${DOTFILES}/vimrc" "${HOME}/.vimrc"
 ln -s "${DOTFILES}/jshintrc" "${HOME}/.jshintrc"
+
+if [ ! -d "${HOME}/bin" ]; then
+  mkdir -p "${HOME}/bin";
+fi
+
+ln -s "${DOTFILES}/aliases" "${HOME}/aliases"
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
