bfeafffdb21233f5558426eb0c50ee2e8a06e79a
Mike Ball <mikedball@gmail.com>
2021-04-25 08:26:01 -0400

    configure nvim to read ~/.vim* configuration
    https://neovim.io/doc/user/nvim.html#nvim-from-vim

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 
 brew bundle
 
-if [ ! "${HOME}/.oh-my-zsh" ]; then
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
@@ -18,12 +18,17 @@ mv "${HOME}/.zshrc" "${HOME}/.zshrc.bak" 2>/dev/null
 
 DOTFILES=${HOME}/dotfiles
 
+if [ ! -d "${HOME}/.config/nvim" ]; then
+  mkdir -p "${HOME}/.config/nvim";
+fi
+
 ln -s "${DOTFILES}/zshrc" "${HOME}/.zshrc"
 ln -s "${DOTFILES}/oh-my-zsh/themes/mdb.zsh-theme" "${HOME}/.oh-my-zsh/themes/mdb.zsh-theme"
 ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
 ln -s "${DOTFILES}/gitconfig" "${HOME}/.gitconfig"
 ln -s "${DOTFILES}/vimrc" "${HOME}/.vimrc"
 ln -s "${DOTFILES}/jshintrc" "${HOME}/.jshintrc"
+ln -s "${DOTFILES}/init.vim" "${HOME}/.config/nvim/init.vim"
 
 if [ ! -d "${HOME}/bin" ]; then
   mkdir -p "${HOME}/bin";
